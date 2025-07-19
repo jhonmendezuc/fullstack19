e2575ab48a422d93cb53878977fa9d5d4e1cbc2d
jhon mendez <programacioncolaborativa22@gmail.com>
2025-07-18 20:28:15 -0500

    feat: add examples node v2

---
+++ 5.NODE_EJEMPLOS/modulos_terceros/index.js
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log(process.env.PUERTO);
+console.log(process.env.URL_BACKEND);
+console.log(process.env.URL_BASE_DATOS);
---
+++ 5.NODE_EJEMPLOS/modulos_terceros/index2.js
@@ -0,0 +1,5 @@
+console.log("proceso 1");
+console.log("proceso 1");
+console.log("proceso 1");
+console.log("proceso 1");
+console.log("proceso 1");
